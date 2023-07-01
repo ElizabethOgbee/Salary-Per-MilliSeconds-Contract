fd0febf07373e430dfbb3d1e397fa84bf445b216
ElizabethOgbee <ogbeititi345@gmail.com>
2023-07-01 17:19:28 +0100

    fixes made

--- contracts/Methereum.sol
+++ contracts/Methereum.sol
@@ -3,17 +3,18 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-contract Methereum is ERC20, ERC20Detailed, Ownable{
 
-    constructor() ERC20Detailed("Methereum", "Mth"){
-       uint256 totalSupply = 1000000 * 10 ** decimals();
-       _mint(msg.sender, totalSupply);
+contract Methereum is ERC20, Ownable {
+    constructor() ERC20("Methereum", "Mth")  {
+        uint256 totalSupply = 1000000 * 10 ** decimals();
+        _mint(msg.sender, totalSupply);
     }
 
-    function transfer(address to, uint256 amount) external admin{
-        _transfer(msg.sender, to, amount);
+    // function transfer(address reciever, uint256 amount) public override onlyOwner {
+    //     _transfer(msg.sender, reciever, amount);
+    // }
+    function transfer( address to, uint256 amount) public {
+     transfer(msg.sender, to, amount);
     }
-
 }
